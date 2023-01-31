e1f9f7a086b1e18304a7adcd31b53f9d6ece73aa
Quentin Inkling <mbee@protonmail.ch>
2023-02-01 01:21:22 +0300

    UX improvements

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Iroha squash (and in the future upgrade) tool
 
-Usage: `cargo run -- --store /path/to/store > genesis.json`
+For usage: `cargo run help`
 
 Known limitations:
-- Only pre-rc-9 is supported at the moment
+- Only pre-rc-9 and pre-rc-11 is supported at the moment
 - Information about asset definition registrar is not retained after squash due to Iroha genesis limitations
+- Build times (there's no easy way to do this without essentialy building all supported versions of iroha)
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -29,8 +29,31 @@ impl Version {
     }
 }
 
-#[derive(FromArgs)]
-/// Squash iroha store
+#[derive(FromArgs, PartialEq, Debug)]
+/// Iroha squash tool - merge (and upgrade) Iroha 2 block stores
+struct Args {
+    #[argh(subcommand)]
+    command: Command,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum Command {
+    Squash(Squash),
+    Versions(Versions),
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "versions")]
+/// List supported iroha versions
+struct Versions {}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "squash")]
+/// Squash iroha block store to a single genesis block
+/// Outputs genesis json representation to stdout.
+/// Example usage:
+/// `cargo run -- squash -s /path/to/store -v pre-rc-9 > genesis.json`
 struct Squash {
     /// path to block store
     #[argh(option, short = 's', long = "store")]
@@ -40,20 +63,17 @@ struct Squash {
     version: String,
 }
 
-fn main() -> anyhow::Result<()> {
-    let args: Squash = argh::from_env();
-
+fn squash(args: Squash) -> anyhow::Result<()> {
     let ver = Version::load(
         VERSIONS
             .get(args.version.as_str())
             .expect("Unsupported version"),
-    )
-    .unwrap();
+    )?;
 
     unsafe {
         let squash_store: Symbol<unsafe extern "C" fn(*const libc::c_char) -> *mut u8> =
             ver.lib.get(b"squash_store")?;
-        let free_str: Symbol<unsafe extern "C" fn(*mut u8)> = ver.lib.get(b"free_str").unwrap();
+        let free_str: Symbol<unsafe extern "C" fn(*mut u8)> = ver.lib.get(b"free_str")?;
         let s = CString::new(args.store)?;
         let res = squash_store(s.as_c_str().as_ptr());
         println!("{}", CStr::from_ptr(res).to_str()?);
@@ -61,4 +81,19 @@ fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let args: Args = argh::from_env();
+
+    match args.command {
+        Command::Squash(args) => squash(args),
+        Command::Versions(_) => {
+            println!(
+                "Supported iroha versions: \n\t{}",
+                VERSIONS.keys().cloned().collect::<Vec<_>>().join("\n\t")
+            );
+            Ok(())
+        }
+    }
 }
